fb3fa2f81a68a3dae29689cda0860a38d8e09936
code_kareem <seniorteck4real@gmail.com>
2021-04-21 12:44:04 -0400

    first commit-nProgress

--- game.js
+++ game.js
@@ -1,2 +1,25 @@
 // alert('callingthefunction')
 
+// $('button').hasClass('gree');
+
+
+
+const buttonColours = ['green', 'red', 'yellow', 'blue'];
+
+const randomChosenColour = buttonColours[nextSequence()];
+
+const gamePattern = [].push(randomChosenColour)
+
+
+// console.log(randomChosenColour);
+
+function nextSequence() {
+    let randomnumber = Math.floor(Math.random()*4);
+    return randomnumber;
+}
+
+// const randomchecker = nextSequence();
+
+// console.log(randomchecker);
+
+$('')
